2b43852b5821ad63eb2ff6771ad884144874ebbd
Ahmed <anashaat95@gmail.com>
2024-08-26 09:14:01 +0300

    Mapping the generic response status code with the status code of ApiControllerBase

---
+++ SchoolApp.API/Bases/AppControllerBase.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SchoolApp.Application.Core.Bases;
+
+namespace SchoolApp.API.Bases;
+
+public class AppControllerBase : ControllerBase
+{
+    #region Fields
+    private IMediator _mediateInstance;
+    protected IMediator Mediator => _mediateInstance ??= HttpContext.RequestServices.GetService<IMediator>();
+    #endregion
+
+    public ObjectResult NewResult<T>(Response<T> response)
+    {
+        switch (response.StatusCode)
+        {
+            case System.Net.HttpStatusCode.OK:
+                return new OkObjectResult(response);
+            case System.Net.HttpStatusCode.Created:
+                return new CreatedResult(string.Empty, response);
+            case System.Net.HttpStatusCode.Unauthorized:
+                return new UnauthorizedObjectResult(response);
+            case System.Net.HttpStatusCode.BadRequest:
+                return new BadRequestObjectResult(response);
+            case System.Net.HttpStatusCode.NotFound:
+                return new NotFoundObjectResult(response);
+            case System.Net.HttpStatusCode.Accepted:
+                return new AcceptedResult(string.Empty, response);
+            case System.Net.HttpStatusCode.UnprocessableEntity:
+                return new UnprocessableEntityObjectResult(response);
+            default:
+                return new BadRequestObjectResult(response);
+        }
+    }
+}
--- SchoolApp.API/Controllers/StudentController.cs
+++ SchoolApp.API/Controllers/StudentController.cs
@@ -1,50 +1,40 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using SchoolApp.API.Bases;
 using SchoolApp.Application.Core.Features.StudentFeature.Commands;
 using SchoolApp.Application.Core.Features.StudentFeature.Queries;
-using SchoolApp.Application.Core.Features.StudentFeature.Queries.GetSignleStudentById;
 using SchoolApp.Application.Core.Features.StudentFeature.Queries.StudentListQuery;
 using SchoolApp.Domain.AppMetaData;
-using SchoolApp.Domain.Entities;
 
 namespace School.API.Controllers
 {
     [Route("")]
     [ApiController]
-    public class StudentController : ControllerBase
+    public class StudentController : AppControllerBase
     {
-        private readonly IMediator _mediator;
-        public StudentController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
         // GET api/<StudentController>/list
         [HttpGet(Router.StudentRoute.List)]
         public async Task<IActionResult> Get()
         {
-            var response = await _mediator.Send(new GetStudentListQueryRequest());
-            return Ok(response);
+            var response = await Mediator.Send(new GetStudentListQueryRequest());
+            return NewResult(response);
         }
 
         // GET api/<StudentController>/5
         [HttpGet(Router.StudentRoute.GetById)]
         public async Task<IActionResult> Get([FromRoute] int id)
         {
-            var response = await _mediator.Send(new GetSignleStudentByIdQueryRequest(id));
-            return Ok(response);
+            var response = await Mediator.Send(new GetSignleStudentByIdQueryRequest(id));
+            return NewResult(response);
         }
 
         // POST api/<StudentController>/Create
         [HttpPost(Router.StudentRoute.Create)]
         public async Task<IActionResult> Post([FromBody] AddStudentCommandRequest command)
         {
-            if (command == null)
-            {
-                return BadRequest("Invalid data sent!");
-            }
-            var response = await _mediator.Send(command);
-            return Ok(response);
+            var response = await Mediator.Send(command);
+            return NewResult(response);
         }
 
         // PUT api/<StudentController>/5
